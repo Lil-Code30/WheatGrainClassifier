8622348fe5b2cfaa98d44c72054d56cabbb88af3
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-02-17 20:06:03 -0500

    refactor : set the resultHistory attributes to public

--- models/ResultHistory.cs
+++ models/ResultHistory.cs
@@ -2,12 +2,12 @@ namespace WheatGrainClassifier.models;
 
 public class ResultHistory
 {
-    private int K {get; }
-    private string DistanceMetric { get; }
-    private DateTime CreatedAt {get; }
-    private List<WheatGrain> TestData { get; }
-    private List<WheatGrain> TrainingData { get; }
-    private double Accuracy { get; }
+    public int K {get; set; }
+    public string DistanceMetric { get; set; }
+    public DateTime CreatedAt {get; set;}
+    public List<WheatGrain> TestData { get; set;}
+    public List<WheatGrain> TrainingData { get; set; }
+    public double Accuracy { get; set; }
     // private List<WheatGrain> PredictedResult { get; }
 
     public ResultHistory(int k, string distanceMetric, List<WheatGrain> testData,  List<WheatGrain> trainingData, double accuracy)
